e9d8fa2d636f2418b7438315ebd918fa09828845
Amine <84468278+amineatm@users.noreply.github.com>
2025-05-27 20:16:51 -0500

    assignment 2_2

--- LESSON_2_PRACTICE/Assignment2_2/main.java
+++ LESSON_2_PRACTICE/Assignment2_2/main.java
@@ -2,6 +2,10 @@ package Assignment2_2;
 
 public class main {
     public static void main(String[] args) {
+        int x = RandomNumbers.getRandomInt(1,9);
+        int y = RandomNumbers.getRandomInt(3,14);
 
+        System.out.println(Math.pow(Math.PI,x));
+        System.out.println(Math.pow(y,Math.PI));
     }
 }
