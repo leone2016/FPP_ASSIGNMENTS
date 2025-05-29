f10125ae1d7d3051c65cbb460804db2516d31867
selome27 <73641796+selome27@users.noreply.github.com>
2025-05-28 21:55:10 -0500

    Add files via upload

---
+++ LESSON_3_PRACTICE/Assignment3_4/Circle.java
@@ -0,0 +1,15 @@
+package Prog3_4;
+
+public final class Circle {
+    private final double  radius;
+    public Circle(double radius){
+        this.radius = radius;
+    }
+    public double getRadius(){
+        return radius;
+    }
+    public double computeArea(){
+
+        return Math.PI * radius * radius;
+    }
+}
---
+++ LESSON_3_PRACTICE/Assignment3_4/Rectangle.java
@@ -0,0 +1,20 @@
+package Prog3_4;
+
+public  class Rectangle {
+    private final double width;
+    private final double length;
+
+    public Rectangle(double width, double length) {
+
+        this.width = width;
+        this.length = length;
+    }
+    public double getWidth(){return width;}
+
+    public double getLength(){return length;}
+    public double computeArea() {
+
+
+        return width * length;
+    }
+}
