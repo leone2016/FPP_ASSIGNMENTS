2d3da496a2c3f156b66e767eb2a76b48f903a1ac
selome27 <selometas21@gmail.com>
2025-05-31 08:40:59 -0500

    Program4_2

--- LESSON_4_PRACTICE/src/ASSIGNMENT4_2/Test.java
+++ LESSON_4_PRACTICE/src/ASSIGNMENT4_2/Test.java
@@ -7,10 +7,12 @@ public class Test {
         ClosedCurve[] objects = { new Triangle(4, 5, 6), new Square(3), new Circle(3), new Rectangle(4, 2) };
         // compute areas
         for (ClosedCurve cc : objects) {
-            System.out.println("The area of this " + cc.getClass().getSimpleName() + " is :" + cc.computeArea());
-
+            String nameOfCurve = cc.getClass().getSimpleName();
+            System.out.printf("The area of this %s is %.2f \n",
+                    nameOfCurve, cc.computeArea());
         }
-
     }
-
 }
+
+
+
