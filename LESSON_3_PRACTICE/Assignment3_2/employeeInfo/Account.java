21f6aa6b52f57d32e7ea8798259a415be72ce3ea
selome27 <selometas21@gmail.com>
2025-06-02 16:00:03 -0500

    my second commit

--- LESSON_3_PRACTICE/Assignment3_2/employeeInfo/Account.java
+++ LESSON_3_PRACTICE/Assignment3_2/employeeInfo/Account.java
@@ -1,45 +1,45 @@
-package Assignment3_2.employeeInfo;
-
-class Account {
-	private final static double DEFAULT_BALANCE = 0.0;
-
-	private double balance;
-	private AccountType acctType;
-	private Employee employee;
-
-	// Constructor
-	Account(Employee emp, AccountType acctType, double balance) {
-		employee = emp;
-		this.acctType = acctType;
-		this.balance = balance;
-	}
-
-	Account(Employee emp, AccountType acctType) {
-		this(emp, acctType, DEFAULT_BALANCE);
-	}
-
-	public String toString() {
-		return "Account type: " + acctType.toString().toLowerCase() + "\n" +
-				"Current bal:  " + balance;
-	}
-
-	public void makeDeposit(double deposit) {
-		if (deposit < 0) {
-			throw new IllegalArgumentException("Deposit amount must be non-negative");
-		}
-		balance += deposit; // update balance
-		System.out.println("Deposit successful. New balance: " + balance);
-	}
-
-	public boolean makeWithdrawal(double amount) {
-		if (amount > balance) {
-			return false; // withdrawal amount too large
-		} else {
-			balance -= amount; // update balance
-			return true; // successful withdrawal
-		}
-	}
-	public double getBalance() {
-		return balance;
-	}
-}
+//package Assignment3_2.employeeInfo;
+//
+//class Account {
+//	private final static double DEFAULT_BALANCE = 0.0;
+//
+//	private double balance;
+//	private AccountType acctType;
+//	private Employee employee;
+//
+//	// Constructor
+//	Account(Employee emp, AccountType acctType, double balance) {
+//		employee = emp;
+//		this.acctType = acctType;
+//		this.balance = balance;
+//	}
+//
+//	Account(Employee emp, AccountType acctType) {
+//		this(emp, acctType, DEFAULT_BALANCE);
+//	}
+//
+//	public String toString() {
+//		return "Account type: " + acctType.toString().toLowerCase() + "\n" +
+//				"Current bal:  " + balance;
+//	}
+//
+//	public void makeDeposit(double deposit) {
+//		if (deposit < 0) {
+//			throw new IllegalArgumentException("Deposit amount must be non-negative");
+//		}
+//		balance += deposit; // update balance
+//		System.out.println("Deposit successful. New balance: " + balance);
+//	}
+//
+//	public boolean makeWithdrawal(double amount) {
+//		if (amount > balance) {
+//			return false; // withdrawal amount too large
+//		} else {
+//			balance -= amount; // update balance
+//			return true; // successful withdrawal
+//		}
+//	}
+//	public double getBalance() {
+//		return balance;
+//	}
+//}
--- LESSON_4_DOC/lab4_2/closedcurve/good/Circle.java
+++ LESSON_4_DOC/lab4_2/closedcurve/good/Circle.java
@@ -1,11 +1,11 @@
-package lesson4.lab4_2.closedcurve.good;
-
-public final class Circle extends ClosedCurve {
-	private final double radius;
-	public Circle(double radius) {
-		this.radius = radius;
-	}
-	public double computeArea() {
-		return (Math.PI * radius * radius);
-	}
-}
+//package lesson4.lab4_2.closedcurve.good;
+//
+//public final class Circle extends ClosedCurve {
+//	private final double radius;
+//	public Circle(double radius) {
+//		this.radius = radius;
+//	}
+//	public double computeArea() {
+//		return (Math.PI * radius * radius);
+//	}
+//}
