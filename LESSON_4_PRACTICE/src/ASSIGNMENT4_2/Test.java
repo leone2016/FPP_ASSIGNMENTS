f03023af7398f51fcdedeefa2bac3586a0133676
Leonardo Medina <leoz.31@hotmail.com>
2025-06-07 08:39:51 -0500

    ffff

--- LESSON_4_PRACTICE/src/ASSIGNMENT4_2/Test.java
+++ LESSON_4_PRACTICE/src/ASSIGNMENT4_2/Test.java
@@ -4,7 +4,11 @@ public class Test {
 
     public static void main(String[] args) {
 
-        ClosedCurve[] objects = { new Triangle(4, 5, 6), new Square(3), new Circle(3), new Rectangle(4, 2) };
+        ClosedCurve[] objects = { 
+            new Triangle(4, 5, 6),
+            new Square(3),
+            new Circle(3),
+            new Rectangle(4, 2) };
         // compute areas
         for (ClosedCurve cc : objects) {
             String nameOfCurve = cc.getClass().getSimpleName();
