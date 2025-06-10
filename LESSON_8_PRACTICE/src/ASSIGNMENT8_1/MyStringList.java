14290b911cd673057505e9196e3f4ea00f012726
Amine <84468278+amineatm@users.noreply.github.com>
2025-06-09 20:35:37 -0500

    assignment 8-2

--- LESSON_8_PRACTICE/src/ASSIGNMENT8_1/MyStringList.java
+++ LESSON_8_PRACTICE/src/ASSIGNMENT8_1/MyStringList.java
@@ -1,126 +1,150 @@
 package ASSIGNMENT8_1;
 
 public class MyStringList {
-	private final int INITIAL_LENGTH = 4;
-	private String[] strArray; 
-	private int size;
-	
-	public MyStringList() {
-		strArray = new String[INITIAL_LENGTH];
-		size = 0;
-	}
-	
-	/******* sorting methods ********/
-	public void minSort(){
-		//implement
-	}
-	void swap(int i, int j){
-		//implement
-		
-	}
-	//find minimum of arr between the indices bottom and top
-	public int minpos(int bottom, int top){		
-		//implement
-		return -1;
-	}
+    private final int INITIAL_LENGTH = 4;
+    private String[] strArray;
+    private int size;
 
-	/********* end sorting methods ********/
-	
-	/******** binary search methods ********/
-	
-	//search a sorted array
-	boolean binSearchIt(String val) {
-	    //implement
-		return false;
-	}
+    public MyStringList() {
+        strArray = new String[INITIAL_LENGTH];
+        size = 0;
+    }
 
-	/********* end binary search methods *****/
-	
-	public void add(String s){
-		if(size == strArray.length) resize();
-		strArray[size++] = s;
-	}
-	
-	public String get(int i){
-		if(i < 0 || i >= size){
-			return null;
-		}
-		return strArray[i];
-	}
-	
-	public boolean find(String s){
-		for(String test : strArray){
-			if(test.equals(s)) return true;
-		}
-		return false;
-	}
-	
-	public void insert(String s, int pos){
-		if(pos > size) return;
-		if(pos >= strArray.length||size+1 > strArray.length) {
-			resize();
-		}
-		String[] temp = new String[strArray.length+1];
-		System.arraycopy(strArray,0,temp,0,pos);
-		temp[pos] = s;
-		
-		System.arraycopy(strArray,pos,temp,pos+1, strArray.length - pos);
-		strArray = temp;
-		++size;
-		
-	}
-	
-	public boolean remove(String s){
-		if(size == 0) return false;
-		int index = -1;
-		for(int i = 0; i < size; ++i ){
-			if(strArray[i].equals(s)){
-				index = i;
-				break;
-			}
-		}
-		if(index==-1) return false;
-		String[] temp = new String[strArray.length];
-		System.arraycopy(strArray,0,temp,0,index);
-		System.arraycopy(strArray,index+1,temp,index,strArray.length-(index+1));
-		strArray = temp;
-		--size;
-		return true;
-	}
-	
-	
-	private void resize(){
-		System.out.println("resizing");
-		int len = strArray.length;
-		int newlen = 2*len;
-		String[] temp = new String[newlen];
-		System.arraycopy(strArray,0,temp,0,len);
-		strArray = temp;
-	}
-	public String toString(){
-		StringBuilder sb = new StringBuilder("[");
-		for(int i = 0; i < size-1; ++i){
-			sb.append(strArray[i]+", ");
-		}
-		sb.append(strArray[size-1]+"]");
-		return sb.toString();
-	}
-	public int size() {
-		return size;
-	}
+    /******* sorting methods ********/
+    public void minSort() {
+        for (int i = 0; i < size - 1; i++) {
+            int nextMinPos = minpos(i, size - 1);
+            swap(i, nextMinPos);
+        }
+    }
 
-	public static void main(String[] args){
-		String[] testArr = {"big", "small", "tall", "short", "round", "square", "enormous", "tiny","gargantuan", "lilliputian", "numberless", "none", "vast", "miniscule"};
-		MyStringList list = new MyStringList();
-		for(int i = 0; i < testArr.length; ++i) {
-			list.add(testArr[i]);
-		}
-		list.minSort();
-		System.out.println("The list in sorted order:");
-		System.out.println(list.toString());
-		System.out.println();
-		System.out.println("\"number\" is in the list? "+list.binSearchIt("number"));
-		System.out.println("\"tiny\" is in the list? "+list.binSearchIt("tiny"));
-	}
+    void swap(int i, int j) {
+        String tmp = strArray[i];
+        strArray[i] = strArray[j];
+        strArray[j] = tmp;
+    }
+
+    //find minimum of arr between the indices bottom and top
+    public int minpos(int bottom, int top) {
+        if (bottom < 0 || top >= size || bottom > top) return -1;
+
+        int minPos = bottom;
+        for (int i = bottom + 1; i <= top; i++) {
+            if (strArray[i].compareTo(strArray[minPos]) < 0) {
+                minPos = i;
+            }
+        }
+        if (minPos >= bottom && minPos <= top) return minPos;
+        return -1;
+    }
+
+    /********* end sorting methods ********/
+
+    /******** binary search methods ********/
+
+    //search a sorted array
+    boolean binSearchIt(String val) {
+        int low = 0;
+        int high = size - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int cmp = strArray[mid].compareTo(val);
+            if (cmp == 0) return true;
+            else if (cmp < 0) low = mid + 1;
+            else high = mid - 1;
+        }
+        return false;
+    }
+
+    /********* end binary search methods *****/
+
+    public void add(String s) {
+        if (size == strArray.length) resize();
+        strArray[size++] = s;
+    }
+
+    public String get(int i) {
+        if (i < 0 || i >= size) {
+            return null;
+        }
+        return strArray[i];
+    }
+
+    public boolean find(String s) {
+        for (String test : strArray) {
+            if (test.equals(s)) return true;
+        }
+        return false;
+    }
+
+    public void insert(String s, int pos) {
+        if (pos > size) return;
+        if (pos >= strArray.length || size + 1 > strArray.length) {
+            resize();
+        }
+        String[] temp = new String[strArray.length + 1];
+        System.arraycopy(strArray, 0, temp, 0, pos);
+        temp[pos] = s;
+
+        System.arraycopy(strArray, pos, temp, pos + 1, strArray.length - pos);
+        strArray = temp;
+        ++size;
+
+    }
+
+    public boolean remove(String s) {
+        if (size == 0) return false;
+        int index = -1;
+        for (int i = 0; i < size; ++i) {
+            if (strArray[i].equals(s)) {
+                index = i;
+                break;
+            }
+        }
+        if (index == -1) return false;
+        String[] temp = new String[strArray.length];
+        System.arraycopy(strArray, 0, temp, 0, index);
+        System.arraycopy(strArray, index + 1, temp, index, strArray.length - (index + 1));
+        strArray = temp;
+        --size;
+        return true;
+    }
+
+
+    private void resize() {
+        System.out.println("resizing");
+        int len = strArray.length;
+        int newlen = 2 * len;
+        String[] temp = new String[newlen];
+        System.arraycopy(strArray, 0, temp, 0, len);
+        strArray = temp;
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder("[");
+        for (int i = 0; i < size - 1; ++i) {
+            sb.append(strArray[i] + ", ");
+        }
+        sb.append(strArray[size - 1] + "]");
+        return sb.toString();
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public static void main(String[] args) {
+        String[] testArr = {"big", "small", "tall", "short", "round", "square", "enormous", "tiny", "gargantuan", "lilliputian", "numberless", "none", "vast", "miniscule"};
+        MyStringList list = new MyStringList();
+        for (int i = 0; i < testArr.length; ++i) {
+            list.add(testArr[i]);
+        }
+        list.minSort();
+        System.out.println("The list in sorted order:");
+        System.out.println(list.toString());
+        System.out.println();
+        System.out.println("\"number\" is in the list? " + list.binSearchIt("number"));
+        System.out.println("\"tiny\" is in the list? " + list.binSearchIt("tiny"));
+    }
 
 }
