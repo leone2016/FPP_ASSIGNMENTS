b6ee9adcf1703b7a96c7286945454f1256c33459
Leonardo Medina <leoz.31@hotmail.com>
2025-06-10 15:05:53 -0500

    feat(assignment8) practice 5

---
+++ LESSON_8_PRACTICE/src/ASSIGNMENT8_5/Main.java
@@ -0,0 +1,14 @@
+package ASSIGNMENT8_5;
+
+public class Main {
+    public static void main(String[] args) {
+        MyTable t = new MyTable();
+        t.add('a', "Andrew");
+        t.add('b',"Billy");
+        t.add('c',"Charlie");
+        String s = t.get('b');
+        String z = t.get('z');
+        System.out.println(s); // Billy
+        System.out.println(z); // null
+    }
+}
